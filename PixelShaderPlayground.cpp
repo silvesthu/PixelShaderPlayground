e70abdd615099603e1e4035fc36460557b8ac9f2
Ken Hu <silvesthu@gmail.com>
2024-05-25 05:46:45 +0900

    Add SwapChain for capture, add one more shader for comparison

--- PixelShaderPlayground.cpp
+++ PixelShaderPlayground.cpp
@@ -1,28 +1,46 @@
 #include <string>
+#include <algorithm>
 
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #include <dxcapi.h>
+#include <dxgidebug.h>
 #include <d3d12shader.h>
+#include <pix3.h>
 
 #include <wrl.h>
 using namespace Microsoft::WRL;
 
+#undef min
+#undef max
+
 extern "C" { __declspec(dllexport) extern const UINT			D3D12SDKVersion = 614; }
 extern "C" { __declspec(dllexport) extern const char8_t*		D3D12SDKPath = u8".\\D3D12\\"; }
 
-int main()
+static const wchar_t* kApplicationTitleW		= L"Pixel Shader Playground";
+static const uint32_t kFrameCount				= 16;
+static const uint32_t kTargetSizeX				= 4096;
+static const uint32_t kTargetSizeY				= 4096;
+static const bool kPrintDisassembly				= false;
+
+static LRESULT WINAPI sWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	const uint32_t kTargetSizeX					= 8;
-	const uint32_t kTargetSizeY					= 4;
+	switch (msg)
+	{
+		case WM_DESTROY:
+			::PostQuitMessage(0);
+			return 0;
+	}
 
-	const bool kPrintDisassembly				= false;
+	return ::DefWindowProc(hWnd, msg, wParam, lParam);
+}
 
-	//////////////////////////////////////////////////////////////////////////
-
+int Run(HWND hwnd)
+{
 	ComPtr<ID3D12Debug> d3d12_debug;
 	D3D12GetDebugInterface(IID_PPV_ARGS(&d3d12_debug));
-	d3d12_debug->EnableDebugLayer();
+	if (GetModuleHandleA("Nvda.Graphics.Interception.dll") == NULL)
+		d3d12_debug->EnableDebugLayer();
 
 	ComPtr<IDXGIFactory4> factory;
 	CreateDXGIFactory2(0, IID_PPV_ARGS(&factory));
@@ -39,10 +57,9 @@ int main()
 	D3D12_FEATURE_DATA_D3D12_OPTIONS1 options1 = {};
 	device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS1, &options1, sizeof(D3D12_FEATURE_DATA_D3D12_OPTIONS1));
 
-	//////////////////////////////////////////////////////////////////////////
-
 	ComPtr<IDxcBlob> vs_blob;
-	ComPtr<IDxcBlob> ps_blob;
+	ComPtr<IDxcBlob> ps_blob_a;
+	ComPtr<IDxcBlob> ps_blob_b;
 	{
 		ComPtr<IDxcLibrary> library;
 		DxcCreateInstance(CLSID_DxcLibrary, IID_PPV_ARGS(&library));
@@ -83,44 +100,55 @@ int main()
 		printf("\n");
 		ComPtr<IDxcIncludeHandler> dxc_include_handler;
 		utils->CreateDefaultIncludeHandler(&dxc_include_handler);
-		if (FAILED(compiler->Compile(source_blob.Get(), L"Shader.hlsl", L"vs_main", L"vs_6_7", arguments, _countof(arguments), defines, _countof(defines), dxc_include_handler.Get(), &result)) ||
-			FAILED(result->GetResult(&vs_blob)))
+
+		auto check_result = [](ComPtr<IDxcOperationResult> result)
 		{
-			printf("Vertex shader compile failed\n");
+			HRESULT compile_result;
+			result->GetStatus(&compile_result);
+
+			ComPtr<IDxcBlobEncoding> error_blob;
+			if (SUCCEEDED(result->GetErrorBuffer(&error_blob)) && error_blob != nullptr)
+			{
+				std::string message((const char*)error_blob->GetBufferPointer(), error_blob->GetBufferSize());
+				printf("%s", message.c_str());
+				printf("\n");
+			}
+			return SUCCEEDED(compile_result);
+		};
+
+		auto print_reflection = [&](ComPtr<IDxcBlob> blob)
+		{
+			DxcBuffer dxc_buffer{ .Ptr = blob->GetBufferPointer(), .Size = blob->GetBufferSize(), .Encoding = DXC_CP_ACP };
+			ComPtr<ID3D12ShaderReflection> shader_reflection;
+			utils->CreateReflection(&dxc_buffer, IID_PPV_ARGS(&shader_reflection));
+			UINT64 shader_required_flags = shader_reflection->GetRequiresFlags();
+			printf("D3D_SHADER_REQUIRES_WAVE_OPS = %d\n", (shader_required_flags & D3D_SHADER_REQUIRES_WAVE_OPS) ? 1 : 0);
+			printf("D3D_SHADER_REQUIRES_DOUBLES = %d\n", (shader_required_flags & D3D_SHADER_REQUIRES_DOUBLES) ? 1 : 0);
+			printf("\n");
+
+			ComPtr<IDxcBlobEncoding> disassemble_blob;
+			if (kPrintDisassembly && SUCCEEDED(compiler->Disassemble(blob.Get(), &disassemble_blob)))
+			{
+				std::string message((const char*)disassemble_blob->GetBufferPointer(), disassemble_blob->GetBufferSize());
+				printf("%s", message.c_str());
+				printf("\n");
+			}
+		};
+
+		compiler->Compile(source_blob.Get(), L"Shader.hlsl", L"vs_main", L"vs_6_7", arguments, _countof(arguments), defines, _countof(defines), dxc_include_handler.Get(), &result);
+		result->GetResult(&vs_blob);
+		if (!check_result(result))
 			return 0;
-		}
-		HRESULT hr = compiler->Compile(source_blob.Get(), L"Shader.hlsl", L"ps_main", L"ps_6_7", arguments, _countof(arguments), defines, _countof(defines), dxc_include_handler.Get(), &result);
-		if (SUCCEEDED(hr))
-			result->GetStatus(&hr);
-		bool compile_succeed = SUCCEEDED(hr);
-		ComPtr<IDxcBlobEncoding> error_blob;
-		if (SUCCEEDED(result->GetErrorBuffer(&error_blob)) && error_blob)
-		{
-			printf("Pixel shader compile %s\n", compile_succeed ? "succeed" : "failed");
-			std::string message((const char*)error_blob->GetBufferPointer(), error_blob->GetBufferSize());
-			printf("%s", message.c_str());
-			printf("\n");
-			if (!compile_succeed)
-				return 0;
-		}
-
-		result->GetResult(&ps_blob);
-
-		DxcBuffer dxc_buffer { .Ptr = ps_blob->GetBufferPointer(), .Size = ps_blob->GetBufferSize(), .Encoding = DXC_CP_ACP };
-		ComPtr<ID3D12ShaderReflection> shader_reflection;
-		utils->CreateReflection(&dxc_buffer, IID_PPV_ARGS(&shader_reflection));
-		UINT64 shader_required_flags = shader_reflection->GetRequiresFlags();
-		printf("D3D_SHADER_REQUIRES_WAVE_OPS = %d\n",			(shader_required_flags & D3D_SHADER_REQUIRES_WAVE_OPS) ? 1 : 0);
-		printf("D3D_SHADER_REQUIRES_DOUBLES = %d\n",			(shader_required_flags & D3D_SHADER_REQUIRES_DOUBLES) ? 1 : 0);
-		printf("\n");
-
-		ComPtr<IDxcBlobEncoding> disassemble_blob;
-		if (kPrintDisassembly && SUCCEEDED(compiler->Disassemble(ps_blob.Get(), &disassemble_blob)))
-		{
-			std::string message((const char*)disassemble_blob->GetBufferPointer(), disassemble_blob->GetBufferSize());
-			printf("%s", message.c_str());
-			printf("\n");
-		}
+		compiler->Compile(source_blob.Get(), L"Shader.hlsl", L"ps_main_a", L"ps_6_7", arguments, _countof(arguments), defines, _countof(defines), dxc_include_handler.Get(), &result);
+		result->GetResult(&ps_blob_a);
+		if (!check_result(result))
+			return 0;
+		print_reflection(ps_blob_a);
+		compiler->Compile(source_blob.Get(), L"Shader.hlsl", L"ps_main_b", L"ps_6_7", arguments, _countof(arguments), defines, _countof(defines), dxc_include_handler.Get(), &result);
+		result->GetResult(&ps_blob_b);
+		if (!check_result(result))
+			return 0;
+		print_reflection(ps_blob_b);
 	}
 
 	//////////////////////////////////////////////////////////////////////////
@@ -157,7 +185,7 @@ int main()
 			mReadbackDesc.Flags = D3D12_RESOURCE_FLAG_NONE;
 		}
 
-		D3D12_RESOURCE_DESC mDesc = {};		
+		D3D12_RESOURCE_DESC mDesc = {};
 		D3D12_HEAP_PROPERTIES mProperties = {};
 		ComPtr<ID3D12Resource> mGPUResource;
 
@@ -185,9 +213,27 @@ int main()
 	device->CreateDescriptorHeap(&rtv_heap_desc, IID_PPV_ARGS(&rtv_heap));
 	D3D12_CPU_DESCRIPTOR_HANDLE rtv_handle = rtv_heap->GetCPUDescriptorHandleForHeapStart();
 	device->CreateRenderTargetView(rtv.mGPUResource.Get(), nullptr, rtv_handle);
-	
+
+	ComPtr<ID3D12Resource> srv_resource;
+	D3D12_RESOURCE_DESC srv_resource_desc = rtv.mDesc;
+	device->CreateCommittedResource(&rtv.mProperties, D3D12_HEAP_FLAG_NONE, &srv_resource_desc, D3D12_RESOURCE_STATE_ALL_SHADER_RESOURCE, nullptr, IID_PPV_ARGS(&srv_resource));
+	ComPtr<ID3D12DescriptorHeap> srv_heap;
+	D3D12_DESCRIPTOR_HEAP_DESC srv_heap_desc = {};
+	srv_heap_desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	srv_heap_desc.NumDescriptors = 1;
+	srv_heap_desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	srv_heap_desc.NodeMask = 1;
+	device->CreateDescriptorHeap(&srv_heap_desc, IID_PPV_ARGS(&srv_heap));
+	D3D12_SHADER_RESOURCE_VIEW_DESC srv_desc = {};
+	srv_desc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	srv_desc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+	srv_desc.Texture2D.MipLevels = (UINT)-1;
+	srv_desc.Texture2D.MostDetailedMip = 0;
+	srv_desc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	device->CreateShaderResourceView(nullptr, &srv_desc, srv_heap->GetCPUDescriptorHandleForHeapStart());
+
 	ComPtr<ID3D12RootSignature> root_signature;
-	device->CreateRootSignature(0, ps_blob->GetBufferPointer(), ps_blob->GetBufferSize(), IID_PPV_ARGS(&root_signature));
+	device->CreateRootSignature(0, ps_blob_a->GetBufferPointer(), ps_blob_a->GetBufferSize(), IID_PPV_ARGS(&root_signature));
 
 	D3D12_RASTERIZER_DESC rasterizer_desc = {};
 	rasterizer_desc.FillMode = D3D12_FILL_MODE_SOLID;
@@ -199,8 +245,8 @@ int main()
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC pipeline_state_desc = {};
 	pipeline_state_desc.VS.pShaderBytecode = vs_blob->GetBufferPointer();
 	pipeline_state_desc.VS.BytecodeLength = vs_blob->GetBufferSize();
-	pipeline_state_desc.PS.pShaderBytecode = ps_blob->GetBufferPointer();
-	pipeline_state_desc.PS.BytecodeLength = ps_blob->GetBufferSize();
+	pipeline_state_desc.PS.pShaderBytecode = ps_blob_a->GetBufferPointer();
+	pipeline_state_desc.PS.BytecodeLength = ps_blob_a->GetBufferSize();
 	pipeline_state_desc.pRootSignature = root_signature.Get();
 	pipeline_state_desc.RasterizerState = rasterizer_desc;
 	pipeline_state_desc.BlendState = blend_desc;
@@ -211,8 +257,12 @@ int main()
 	pipeline_state_desc.NumRenderTargets = 1;
 	pipeline_state_desc.RTVFormats[0] = rtv.mDesc.Format;
 	pipeline_state_desc.SampleDesc.Count = 1;
-	ComPtr<ID3D12PipelineState> pso;
-	device->CreateGraphicsPipelineState(&pipeline_state_desc, IID_PPV_ARGS(&pso));
+	ComPtr<ID3D12PipelineState> pso_a;
+	device->CreateGraphicsPipelineState(&pipeline_state_desc, IID_PPV_ARGS(&pso_a));
+	pipeline_state_desc.PS.pShaderBytecode = ps_blob_b->GetBufferPointer();
+	pipeline_state_desc.PS.BytecodeLength = ps_blob_b->GetBufferSize();
+	ComPtr<ID3D12PipelineState> pso_b;
+	device->CreateGraphicsPipelineState(&pipeline_state_desc, IID_PPV_ARGS(&pso_b));
 
 	D3D12_COMMAND_QUEUE_DESC queue_desc = {};
 	queue_desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
@@ -225,78 +275,178 @@ int main()
 
 	ComPtr<ID3D12GraphicsCommandList> command_list;
 	device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, command_allocator.Get(), nullptr, IID_PPV_ARGS(&command_list));
-
-	//////////////////////////////////////////////////////////////////////////
-
-	D3D12_RESOURCE_DESC desc = rtv.mDesc;
-	D3D12_VIEWPORT viewport = {};
-	viewport.Width = static_cast<float>(desc.Width);
-	viewport.Height = static_cast<float>(desc.Height);
-	viewport.MinDepth = 0.0f;
-	viewport.MaxDepth = 1.0f;
-	viewport.TopLeftX = 0.0f;
-	viewport.TopLeftY = 0.0f;
-	command_list->RSSetViewports(1, &viewport);
-	D3D12_RECT rect = {};
-	rect.left = 0;
-	rect.top = 0;
-	rect.right = static_cast<LONG>(desc.Width);
-	rect.bottom = static_cast<LONG>(desc.Height);
-	command_list->RSSetScissorRects(1, &rect);
-	command_list->OMSetRenderTargets(1, &rtv_handle, false, nullptr);
-	command_list->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-	command_list->SetGraphicsRootSignature(root_signature.Get());
-	command_list->SetPipelineState(pso.Get());
-	command_list->DrawInstanced(3, 1, 0, 0);
-
-	D3D12_RESOURCE_BARRIER barriers[1] = {};
-	barriers[0].Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
-	barriers[0].Transition.pResource = rtv.mGPUResource.Get();
-	barriers[0].Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
-	barriers[0].Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
-	barriers[0].Transition.StateAfter = D3D12_RESOURCE_STATE_COPY_SOURCE;
-	command_list->ResourceBarrier(_countof(barriers), &barriers[0]);	
-
-	D3D12_TEXTURE_COPY_LOCATION copy_location_src = {};
-	copy_location_src.pResource = rtv.mGPUResource.Get();
-	copy_location_src.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
-	copy_location_src.SubresourceIndex = 0;
-	D3D12_TEXTURE_COPY_LOCATION copy_location_dst = {};
-	copy_location_dst.pResource = rtv.mReadbackResource.Get();
-	copy_location_dst.Type = D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT;
-	copy_location_dst.PlacedFootprint = rtv.mReadbackLayout;
-	command_list->CopyTextureRegion(&copy_location_dst, 0, 0, 0, &copy_location_src, nullptr);
-
 	command_list->Close();
-
-	ID3D12CommandList* command_lists[] = { command_list.Get() };
-	command_queue->ExecuteCommandLists(_countof(command_lists), command_lists);
 
 	ComPtr<ID3D12Fence> fence;
 	device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence));
-	command_queue->Signal(fence.Get(), 1);
+	HANDLE event_handle = CreateEvent(nullptr, FALSE, FALSE, nullptr);
 
 	//////////////////////////////////////////////////////////////////////////
-	
-	HANDLE handle = CreateEvent(nullptr, FALSE, FALSE, nullptr);
-	fence->SetEventOnCompletion(1, handle);
-	WaitForSingleObject(handle, INFINITE);
 
-	uint8_t* data = nullptr;
-	rtv.mReadbackResource->Map(0, nullptr, (void**)&data);
+	DXGI_SWAP_CHAIN_DESC1 swap_chain_desc = {};
+	ZeroMemory(&swap_chain_desc, sizeof(swap_chain_desc));
+	swap_chain_desc.BufferCount = 2;
+	swap_chain_desc.Width = 0;
+	swap_chain_desc.Height = 0;
+	swap_chain_desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swap_chain_desc.Flags = DXGI_SWAP_CHAIN_FLAG_FRAME_LATENCY_WAITABLE_OBJECT | DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING;
+	swap_chain_desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swap_chain_desc.SampleDesc.Count = 1;
+	swap_chain_desc.SampleDesc.Quality = 0;
+	swap_chain_desc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+	swap_chain_desc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+	swap_chain_desc.Scaling = DXGI_SCALING_STRETCH;
+	swap_chain_desc.Stereo = FALSE;
+	ComPtr<IDXGISwapChain1> swap_chain_1 = nullptr;
+	ComPtr<IDXGISwapChain3> swap_chain = nullptr;
+	factory->CreateSwapChainForHwnd(command_queue.Get(), hwnd, &swap_chain_desc, nullptr, nullptr, &swap_chain_1);
+	swap_chain_1->QueryInterface(IID_PPV_ARGS(&swap_chain));
 
-	int pitch = rtv.mReadbackLayout.Footprint.RowPitch;
-	for (int y = 0; y < kTargetSizeY; y++)
+	::ShowWindow(hwnd, SW_SHOWDEFAULT);
+	::UpdateWindow(hwnd);
+
+	//////////////////////////////////////////////////////////////////////////
+
+	uint32_t frame_index = 0;
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+	while (msg.message != WM_QUIT)
 	{
-		uint8_t* row = (data + pitch * y);
-		for (int x = 0; x < kTargetSizeX; x++)
+		if (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
 		{
-			float* float4 = (float*)(row + x * sizeof(float) * 4);
-			printf("rtv[%d, %d] = %.3f, %.3f, %.3f, %.3f\n", x, y, float4[0], float4[1], float4[2], float4[3]);
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+			continue;
 		}
+
+		command_allocator->Reset();
+		command_list->Reset(command_allocator.Get(), nullptr);
+
+		D3D12_RESOURCE_DESC desc = rtv.mDesc;
+		D3D12_VIEWPORT viewport = {};
+		viewport.Width = static_cast<float>(desc.Width);
+		viewport.Height = static_cast<float>(desc.Height);
+		viewport.MinDepth = 0.0f;
+		viewport.MaxDepth = 1.0f;
+		viewport.TopLeftX = 0.0f;
+		viewport.TopLeftY = 0.0f;
+		command_list->RSSetViewports(1, &viewport);
+		D3D12_RECT rect = {};
+		rect.left = 0;
+		rect.top = 0;
+		rect.right = static_cast<LONG>(desc.Width);
+		rect.bottom = static_cast<LONG>(desc.Height);
+		command_list->SetDescriptorHeaps(1, srv_heap.GetAddressOf());
+		command_list->RSSetScissorRects(1, &rect);
+		command_list->OMSetRenderTargets(1, &rtv_handle, false, nullptr);
+		command_list->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		command_list->SetGraphicsRootSignature(root_signature.Get());
+
+		{
+			PIXScopedEvent(command_list.Get(), PIX_COLOR(255, 0, 0), "ps_main_a 0");
+			command_list->SetPipelineState(pso_a.Get());
+			command_list->DrawInstanced(3, 1, 0, 0);
+		}
+		{
+			PIXScopedEvent(command_list.Get(), PIX_COLOR(0, 255, 0), "ps_main_b 0");
+			command_list->SetPipelineState(pso_b.Get());
+			command_list->DrawInstanced(3, 1, 0, 0);
+		}
+		{
+			PIXScopedEvent(command_list.Get(), PIX_COLOR(255, 0, 0), "ps_main_a 1");
+			command_list->SetPipelineState(pso_a.Get());
+			command_list->DrawInstanced(3, 1, 0, 0);
+		}
+		{
+			PIXScopedEvent(command_list.Get(), PIX_COLOR(0, 255, 0), "ps_main_b 1");
+			command_list->SetPipelineState(pso_b.Get());
+			command_list->DrawInstanced(3, 1, 0, 0);
+		}
+
+		D3D12_RESOURCE_BARRIER barrier = {};
+		barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+		barrier.Transition.pResource = rtv.mGPUResource.Get();
+		barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+		barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+		barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_COPY_SOURCE;
+		command_list->ResourceBarrier(1, &barrier);
+
+		D3D12_TEXTURE_COPY_LOCATION copy_location_src = {};
+		copy_location_src.pResource = rtv.mGPUResource.Get();
+		copy_location_src.Type = D3D12_TEXTURE_COPY_TYPE_SUBRESOURCE_INDEX;
+		copy_location_src.SubresourceIndex = 0;
+		D3D12_TEXTURE_COPY_LOCATION copy_location_dst = {};
+		copy_location_dst.pResource = rtv.mReadbackResource.Get();
+		copy_location_dst.Type = D3D12_TEXTURE_COPY_TYPE_PLACED_FOOTPRINT;
+		copy_location_dst.PlacedFootprint = rtv.mReadbackLayout;
+		command_list->CopyTextureRegion(&copy_location_dst, 0, 0, 0, &copy_location_src, nullptr);
+
+		barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+		barrier.Transition.pResource = rtv.mGPUResource.Get();
+		barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+		barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_SOURCE;
+		barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+		command_list->ResourceBarrier(1, &barrier);
+
+		command_list->Close();
+
+		ID3D12CommandList* command_lists[] = { command_list.Get() };
+		command_queue->ExecuteCommandLists(_countof(command_lists), command_lists);
+
+		swap_chain->Present(1, 0);
+
+		command_queue->Signal(fence.Get(), frame_index);
+		fence->SetEventOnCompletion(frame_index, event_handle);
+		WaitForSingleObject(event_handle, INFINITE);
+
+		if (frame_index == 0)
+		{
+			uint8_t* data = nullptr;
+			rtv.mReadbackResource->Map(0, nullptr, (void**)&data);
+
+			int pitch = rtv.mReadbackLayout.Footprint.RowPitch;
+			for (uint32_t y = 0; y < std::min(kTargetSizeY, 8u); y++)
+			{
+				uint8_t* row = (data + pitch * y);
+				for (uint32_t x = 0; x < std::min(kTargetSizeX, 8u); x++)
+				{
+					float* float4 = (float*)(row + x * sizeof(float) * 4);
+					printf("rtv[%d, %d] = %.3f, %.3f, %.3f, %.3f\n", x, y, float4[0], float4[1], float4[2], float4[3]);
+				}
+			}
+
+			rtv.mReadbackResource->Unmap(0, nullptr);
+		}
+
+		frame_index++;
 	}
 
-	rtv.mReadbackResource->Unmap(0, nullptr);
+	return 0;
+}
+
+int WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, PSTR /*lpCmdLine*/, int /*nCmdShow*/)
+{
+	::AllocConsole();
+	FILE* stream = nullptr;
+	freopen_s(&stream, "CONOUT$", "w", stdout);
+
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, sWndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, kApplicationTitleW, nullptr};
+	::RegisterClassEx(&wc);
+
+	RECT rect = { 0, 0, 400, 400 };
+	AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+	HWND hwnd = ::CreateWindow(wc.lpszClassName, kApplicationTitleW, WS_OVERLAPPEDWINDOW, 100, 100, rect.right - rect.left, rect.bottom - rect.top, nullptr, nullptr, wc.hInstance, nullptr);
+
+	Run(hwnd);
+
+	::DestroyWindow(hwnd);
+	::UnregisterClass(wc.lpszClassName, wc.hInstance);
+
+	::FreeConsole();
+
+	ComPtr<IDXGIDebug1> dxgi_debug;
+	if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgi_debug))))
+		dxgi_debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_FLAGS(DXGI_DEBUG_RLO_DETAIL | DXGI_DEBUG_RLO_IGNORE_INTERNAL));
 
 	return 0;
 }
--- Shader.hlsl
+++ Shader.hlsl
@@ -1,11 +1,10 @@
 
-// Inputs:
-//	THREAD_GROUP_SIZE_X
-//	THREAD_GROUP_SIZE_Y
-//	THREAD_GROUP_SIZE_Z
-//	THREAD_GROUP_SIZE
-//	DISPATCH_SIZE
+// Macro inputs:
+//	TARGET_SIZE_X
+//	TARGET_SIZE_Y
 //	WAVE_LANE_COUNT_MIN
+//  WAVE_LANE_COUNT_MAX
+//  TOTAL_LANE_COUNT
 
 // Generate screen space triangle
 // From https://anteru.net/blog/2012/minimal-setup-screen-space-quads-no-buffers-layouts-required/
@@ -16,8 +15,33 @@ float4 vs_main(uint id : SV_VertexID) : SV_POSITION
 	return float4 (x, y, 0, 1);
 }
 
-[RootSignature("RootFlags(0), UAV(u0)")]
-float4 ps_main(float4 position : SV_POSITION) : SV_Target0
+static Texture2D<float4> Texture = ResourceDescriptorHeap[0];
+SamplerState s0 : register(s0);
+[RootSignature("RootFlags(CBV_SRV_UAV_HEAP_DIRECTLY_INDEXED), StaticSampler(s0, filter=FILTER_MIN_MAG_MIP_LINEAR)")]
+float4 ps_main_a(float4 position : SV_POSITION) : SV_Target0
 {
-	return position;
+    float4 ret      = 0;
+    bool cond       = position.x < 0;
+    [branch]
+    if (QuadAny(cond))
+    {
+        float4 sampled_result = Texture.Sample(s0, (position.xy + 0.5) / float2(TARGET_SIZE_X, TARGET_SIZE_Y));
+        if (cond)
+            ret = sampled_result + 1;
+    }
+    return ret;
+}
+
+float4 ps_main_b(float4 position : SV_POSITION) : SV_Target0
+{
+    float4 ret      = 0;
+    bool cond       = position.x < 0;
+    [branch]
+    if (cond)
+    {
+        float4 sampled_result = Texture.Sample(s0, (position.xy + 0.5) / float2(TARGET_SIZE_X, TARGET_SIZE_Y));
+        if (cond)
+            ret = sampled_result + 1;
+    }
+    return ret;
 }
